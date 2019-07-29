2346ec899a987810a65c7beee062a1e9ee7c995c
HeyIts-RJ <rishab1300@gmail.com>
2019-07-30 03:08:48 +0530

    FCM Setup and Notification working locally.

--- app/src/main/java/com/module/android/notification/MainActivity.java
+++ app/src/main/java/com/module/android/notification/MainActivity.java
@@ -1,13 +1,71 @@
 package com.module.android.notification;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
+    private static final String CHANNEL_ID="APp Name";
+    private static final String CHANNEL_NAME="APP Subname";
+    private static final String CHANNEL_DESC="This is  notification module";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+        }
+        textView=(TextView)findViewById(R.id.token);
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (task.isSuccessful()){
+                            String token = task.getResult().getToken();
+                            textView.setText("TOKEN:"+token);
+                        }else {
+                            textView.setText("Not Generated");
+                        }
+                    }
+                });
+
+
+        findViewById(R.id.buttonNotify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+    }
+
+    private void displayNotification(){
+        NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("It is Working")
+                .setContentText("This is the description content")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat mnotificationmgr=NotificationManagerCompat.from(this);
+        mnotificationmgr.notify(1,mBuilder.build());
     }
 }
